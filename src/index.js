ec0ed05c98e887e21fea6e2fddcf495cb0a062c7
elisiolg <elisiolg@gmail.com>
2024-01-02 21:45:48 +0000

    Changes to be committed: modified:   package-lock.json modified:   package.json new file:   src/actions.js new file:   src/api/api.js new file:   src/constants.js modified:   src/containers/App.js deleted:    src/containers/action.js deleted:    src/containers/constants.js deleted:    src/containers/reducers.js modified:   src/index.js new file:   src/reducers.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import './index.css';
-import App from './containers/App.js'
-import reportWebVitals from './reportWebVitals';
+import { thunk } from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
 
-const store = createStore(rootReducer)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import { requestRobots, searchRobots } from './reducers'
+
+import './index.css';
+
+const logger = createLogger() 
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunk, logger))
+
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Provider store={store}>
+  <App/>
+</Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 reportWebVitals();
